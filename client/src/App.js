dce2f16574473e35dc4b367e46d0c350a041ec41
vinay kumar <vinaykumarhmvini@gmail.com>
2022-04-09 12:19:29 +0530

    first-step-app

---
+++ client/src/App.js
@@ -0,0 +1,16 @@
+import React from 'react'
+import './index.css';
+function App() {
+    return (
+        <div>
+            <header>
+                <a href="/">amzona</a>
+            </header>
+            <div>
+                list of products
+            </div>
+        </div>
+    )
+}
+
+export default App
